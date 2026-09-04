4d58b199b75f342a322aa65e03692e47d6a71ce4
cristianpaes <s4camargo@gmail.com>
2026-09-03 21:46:35 -0300

    Configura PostgreSQL Neon

--- ramais/settings.py
+++ ramais/settings.py
@@ -2,6 +2,7 @@ import os
 
 from pathlib import Path
 
+import dj_database_url
 
 # ============================================================
 # BASE
@@ -109,10 +110,11 @@ WSGI_APPLICATION = "ramais.wsgi.application"
 # ============================================================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
